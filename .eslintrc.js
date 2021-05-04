269a63c3efa4efdfee4f341f82c9590370d438a3
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-05-04 14:06:42 +0200

    initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: ['standard-with-typescript'],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars-experimental': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/prefer-reduce-type-parameter': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    'generator-star-spacing': ['error', { before: false, after: true }],
+    'yield-star-spacing': ['error', { before: false, after: true }],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: false
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false
+        }
+      }
+    ]
+  }
+}
