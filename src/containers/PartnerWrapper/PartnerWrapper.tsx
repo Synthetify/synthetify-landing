21181767171a5a3f0124c1679d464d398ac43114
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-06-04 22:52:08 +0200

    Fix partners positioning

--- src/containers/PartnerWrapper/PartnerWrapper.tsx
+++ src/containers/PartnerWrapper/PartnerWrapper.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IPartnerIProps, Partner } from '@components/Partner/Partner'
+import { Grid } from '@material-ui/core'
 import SolanaLogo from '@static/svg/solana-logo.svg'
 import Alameda from '@static/png/partners/alameda.png'
 import GenblockCapital from '@static/png/partners/genblock-capital.png'
@@ -8,35 +9,36 @@ import CSM from '@static/png/partners/cms-white.png'
 import DefiAlliance from '@static/png/partners/defi-alliance-white.png'
 import DivergenceVentures from '@static/png/partners/divergence-ventures.png'
 import EIVentures from '@static/png/partners/e-i-ventures.png'
+import useStyles from './style'
 
 const partners: IPartnerIProps[] = [
   {
-    logo: SolanaLogo,
-    name: 'Solana'
-  },
-  {
-    logo: Alameda,
-    name: 'Alameda Research'
-  },
-  {
-    logo: GenblockCapital,
-    name: 'Genblock Capital'
-  },
-  {
-    logo: BuidlHodlCapital,
-    name: 'Buidl Hodl Capital'
+    logo: DefiAlliance,
+    name: 'Defi Alliance'
   },
   {
     logo: CSM,
     name: 'CMS'
   },
   {
-    logo: DefiAlliance,
-    name: 'Defi Alliance'
+    logo: BuidlHodlCapital,
+    name: 'Buidl Hodl Capital'
+  },
+  {
+    logo: Alameda,
+    name: 'Alameda Research'
   },
   {
     logo: DivergenceVentures,
     name: 'Divergence Ventures'
+  },
+  {
+    logo: SolanaLogo,
+    name: 'Solana'
+  },
+  {
+    logo: GenblockCapital,
+    name: 'Genblock Capital'
   },
   {
     logo: EIVentures,
@@ -45,8 +47,14 @@ const partners: IPartnerIProps[] = [
 ]
 
 export const PartnerWrapper: React.FC = () => {
+  const classes = useStyles()
+
   const partnersComponents = partners.map((item, index) => {
-    return <Partner key={index} logo={item.logo} name={item.name} maxWidth={item.maxWidth} />
+    return (
+      <Grid item xl={3} lg={4} md={6} className={classes.root}>
+        <Partner key={index} logo={item.logo} name={item.name} maxWidth={item.maxWidth} />
+      </Grid>
+    )
   })
   return <>{partnersComponents}</>
 }
