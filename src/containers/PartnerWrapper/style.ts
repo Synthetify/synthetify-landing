ff45ce617af9cdf85490f155bf851c00b10c5f1d
Mateusz Zając <matzayonc@gmail.com>
2021-07-16 14:57:08 +0200

    linter fixes

--- src/containers/PartnerWrapper/style.ts
+++ src/containers/PartnerWrapper/style.ts
@@ -1,7 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles'
-import { Theme } from '@material-ui/core'
 
-const useStyles = makeStyles((theme: Theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     textAlign: 'center'
   }
