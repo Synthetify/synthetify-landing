0c6043aefc69f2444019d9bfce379d8f4bc14faa
Adrian <adrianwojciak99@gmail.com>
2021-09-07 09:29:34 +0200

    brand

--- src/pages/brand/index.tsx
+++ src/pages/brand/index.tsx
@@ -6,9 +6,11 @@ import React from 'react'
 import greenLogo from '@static/svg/brand/synthetify_logo_green.svg'
 import greenLogoVertical from '@static/svg/brand/synthetify_vertical_logo_green.svg'
 import greenLogoHorizontal from '@static/svg/brand/synthetify_horizontal_logo_green.svg'
+import greenLogoBorder from '@static/svg/brand/synthetify_logo_green_border.svg'
 import whiteLogo from '@static/svg/brand/synthetify_logo_white.svg'
 import whiteLogoVertical from '@static/svg/brand/synthetify_vertical_logo_white.svg'
 import whiteLogoHorizontal from '@static/svg/brand/synthetify_horizontal_logo_white.svg'
+import whiteLogoBorder from '@static/svg/brand/synthetify_logo_white_border.svg'
 import useStyles from '@pagesStyles/brand/style'
 
 export const Brand: React.FC = () => {
@@ -53,6 +55,17 @@ export const Brand: React.FC = () => {
           label={translate('brand.whiteLogoHorizontal')}
           logo={<CardMedia src={whiteLogoHorizontal} className={classes.horizontalLogo} component='img' />}
           filePath='/brand/synthetify_horizontal_logo_white.svg'
+        />
+
+        <LogoTile
+          label={translate('brand.greenLogo')}
+          logo={<CardMedia src={greenLogoBorder} className={classes.simpleLogo} component='img' />}
+          filePath='/brand/synthetify_logo_green_border.svg'
+        />
+        <LogoTile
+          label={translate('brand.whiteLogo')}
+          logo={<CardMedia src={whiteLogoBorder} className={classes.simpleLogo} component='img' />}
+          filePath='/brand/synthetify_logo_white_border.svg'
         />
       </Grid>
     </>
