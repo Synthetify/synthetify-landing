fac4b9c41cdc01f5a88f009c0e43e17c970c1690
Pawel Krawczyk <pawel.krawczyk@interia.com>
2021-09-09 11:01:26 +0200

    change showing 'show more' button depends on height of current answer

--- src/components/Questions/Question.tsx
+++ src/components/Questions/Question.tsx
@@ -1,5 +1,5 @@
 import { Button, Grid, Typography } from '@material-ui/core'
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import useStyles from './style'
 
 interface Props {
@@ -10,19 +10,20 @@ interface Props {
 export const Question = ({ question, answer }: Props) => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const classes = useStyles()
-
+  const ref = useRef<HTMLDivElement>(0)
   const changeHeight = (): void => {
     setIsOpen(!isOpen)
   }
 
   const isVisible = (text: string): boolean => {
-    return typeof text === 'string' ? text.length < 75 : false
+    const height: number = ref.current.scrollHeight
+    return typeof text === 'string' ? height < 85 : false
   }
   return (
     <>
       <Grid container direction='column' classes={{ container: classes.container }}>
         <Typography className={`${classes.text} ${classes.question}`} component='h2'>{question}</Typography>
-        <div className={`${classes.answer} ${isOpen ? classes.open : ''}`}>
+        <div ref={ref} className={`${classes.answer} ${isOpen ? classes.open : ''}`}>
           <p className={classes.text}>{answer}</p>
         </div>
         <Button
@@ -32,6 +33,7 @@ export const Question = ({ question, answer }: Props) => {
           onClick={() => {
             changeHeight()
           }}>
+          {console.log(ref.current.scrollHeight)}
           {isOpen ? 'Show less.' : 'Show more.'}
           <i className={`${classes.arrow} ${isOpen ? classes.arrowUp : classes.arrowDown}`}></i>
         </Button>
