e4c511238664e6916355718d0d85cff90a788178
Adrian <adrianwojciak99@gmail.com>
2021-09-10 13:04:41 +0200

    pdfs moved to public

--- src/static/constants/links.ts
+++ src/static/constants/links.ts
@@ -5,8 +5,8 @@ export const links = {
     exchange: 'https://app.synthetify.io/exchange'
   },
   docs: 'https://docs.synthetify.io/',
-  whitepaper: 'https://resources.synthetify.io/synthetify-whitepaper.pdf',
-  privacyPolicy: 'https://resources.synthetify.io/privacy_policy.pdf',
+  whitepaper: '/resources/synthetify-whitepaper.pdf',
+  privacyPolicy: '/resources/privacy_policy.pdf',
   socialMedia: {
     twitter: 'https://twitter.com/synthetify',
     linkedin: 'https://www.linkedin.com/company/synthetify',
