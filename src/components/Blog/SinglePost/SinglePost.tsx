6c275d4efababb27b4c6c628be43ae89864c09e9
PawelSzydlo <pawel.szydlo.99@gmai.com>
2021-09-14 12:43:05 +0200

    fixed bug with build

--- src/components/Blog/SinglePost/SinglePost.tsx
+++ src/components/Blog/SinglePost/SinglePost.tsx
@@ -4,8 +4,6 @@ import { useTranslate } from '@utils/translations'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import useStyles from './style'
-const id: number = 1
-
 interface IProps{
   singlePost: string
 }
@@ -13,8 +11,8 @@ export const SinglePost: React.FC<IProps> = ({ singlePost }) => {
   const classes = useStyles()
   const translate = useTranslate()
   return (
-    <Grid key={ id } container item justifyContent='center' className={classes.mainContainer}>
-      <ReactMarkdown children={ singlePost } remarkPlugins={[remarkGfm]} />
+    <Grid container item justifyContent='center' className={classes.mainContainer}>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} >{ singlePost }</ReactMarkdown>
       <Button className={classes.backButton}>{translate('blog.backButton')}</Button>
     </Grid>
   )
