78500d54e8b1e6a4bb968cb25d99ae3f42fb7b37
Pawel Krawczyk <pawel.krawczyk@interia.com>
2021-09-14 09:41:59 +0200

    change displaying method of Q&A

--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -9,11 +9,12 @@ const FAQ: React.FC = () => {
   const classes = useStyles()
   const translate = useTranslate()
   const questionsTab: string[] = translate('faq.questions').toString().split(',')
-  const answersTab: string = translate('faq.answers')
+  console.log(questionsTab)
   const questions: JSX.Element[] = questionsTab.map((item, index) => {
+    console.log(translate('faq.questions', index))
     return (<Grid key={index}
       item className={classes.questionBlock}>
-      <Question key={index} question={item} answer={answersTab[index]} />
+      <Question key={index} question={item} answer={translate('faq.answers', index)} />
     </Grid>)
   })
   return (
