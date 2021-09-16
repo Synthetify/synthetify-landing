c561bf4f541b6556405cfee6f26b0fcc0257a901
Adrian <adrianwojciak99@gmail.com>
2021-09-16 11:49:16 +0200

    hello world post

---
+++ src/blogPosts/hello-world.md
@@ -0,0 +1,19 @@
+---
+title: "Hello world!"
+date: "2021-09-16"
+description: "Welcome on our all-new blog!"
+image: "/blog/snyHello.png"
+---
+
+![img1](/blog/snyHello.png)
+
+**Welcome on our all-new blog!**
+
+Here at Synthetify we’re creating a new platform that will help to manage your synthetic assets. Synthetify protocol enables creation, exchange and burn of synthetic assets based on prices provided by a decentralized system of oracles. On Synthetify exchange trading is executed against the public debt pool that allows for almost infinite liquidity and zero slippage even during big trades.
+
+To learn more about Synthetify:
+- visit [our website](https://synthetify.io)
+- read [the plafform docs](https://docs.synthetify.io)
+- keep in touch with us on Twitter: [@synthetify](https://twitter.com/synthetify)
+- Telegram: [t.me/synthetify](https://t.me/synthetify)
+- or Discord: [/Synthetify](https://discord.com/invite/EDrf437)
--- src/components/Blog/SinglePost/SinglePost.stories.tsx
+++ src/components/Blog/SinglePost/SinglePost.stories.tsx
@@ -5,7 +5,15 @@ import { storiesOf } from '@storybook/react'
 storiesOf('blog', module).add('singlepost', () => <SinglePost singlePost={post} title='Welcome' date='2021-01-01' />)
 
 const post: string = `
-![example-Image](https://miro.medium.com/max/1838/0*Pn3ZFJ6ZwnaScnJ-)
+![img1](https://miro.medium.com/max/1838/0*Pn3ZFJ6ZwnaScnJ-)
+**Lorem Ipsum**
+
+Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an
+unknown printer took a **galley of type** and _scrambled_ it to make a [type](wp.pl) specimen book. It
+has survived not only five centuries, but also the leap into electronic typesetting,
+remaining essentially unchanged.
+
+![img2](https://miro.medium.com/max/1838/0*Pn3ZFJ6ZwnaScnJ-)
 **Lorem Ipsum**
 
 Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an
@@ -13,21 +21,13 @@ unknown printer took a galley of type and scrambled it to make a type specimen b
 has survived not only five centuries, but also the leap into electronic typesetting,
 remaining essentially unchanged.
 
-It was popularised in the 1960s with the release of
-    Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+- First item
+- Second item
+- Third item
+- Fourth item 
 
-
-![example-Image](https://miro.medium.com/max/1838/0*Pn3ZFJ6ZwnaScnJ-)
-
-**Header 2**
-
-It has survived not only five centuries, but also the leap into electronic typesetting,
-    remaining essentially unchanged. It was popularised in the 1960s with the release of
-    Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-It was popularised in the 1960s with the release of
-    Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an
+unknown printer took a galley of type and scrambled it to make a type specimen book. It
+has survived not only five centuries, but also the leap into electronic typesetting,
+remaining essentially unchanged.
 `
