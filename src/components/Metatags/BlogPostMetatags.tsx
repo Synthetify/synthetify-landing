26494497b7fa41d3dc288fa3b9b84192159849b5
Adrian <adrianwojciak99@gmail.com>
2021-09-20 12:30:33 +0200

    quicktest

--- src/components/Metatags/BlogPostMetatags.tsx
+++ src/components/Metatags/BlogPostMetatags.tsx
@@ -20,7 +20,7 @@ export const BlogPostMetatags: React.FC<IProps> = ({ slug, title, description, i
     <meta property="og:image" key="og:image" content={image} />
 
     {/* Twitter Card  */}
-    <meta property="twitter:url" key="twitter:url" content='' />
+    <meta property="twitter:url" key="twitter:url" content="https://twitter.com/synthetify" />
     <meta property="twitter:title" key="twitter:title" content={`${title} - Synthetify`} />
     <meta
       property="twitter:description"
