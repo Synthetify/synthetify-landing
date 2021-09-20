927af331e6921d3bc3ec9d90efda886f88fee8df
Adrian <adrianwojciak99@gmail.com>
2021-09-20 09:01:05 +0200

    remove green background in post tile

--- src/components/Blog/Article/style.ts
+++ src/components/Blog/Article/style.ts
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   root: {
     width: '100%',
-    backgroundColor: '#40BFA0',
+    backgroundColor: 'transparent',
     borderRadius: 20,
     display: 'grid',
     overflow: 'hidden',
