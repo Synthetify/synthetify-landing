c90f9d75917f1e6f780161eff938a2b785813d0f
Kamil Cyrek <kamilcyrek@MacBook-Air-Kamil.local>
2021-11-23 10:30:25 +0100

    change styles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,46 @@
 import React from 'react'
-import useStyles from '@pagesStyles/custom404/style'
+import { makeStyles, Theme } from '@material-ui/core/styles'
+import { colors } from '@static/theme'
 import { Grid, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    fontFamily: 'Nunito Sans'
+  },
+  title: {
+    marginTop: 116,
+    fontWeight: 800,
+    fontSize: 70,
+    lineHeight: '49px',
+    color: colors.white.main,
+    [theme.breakpoints.down('xs')]: {
+      marginTop: 96
+    }
+  },
+  subTitle: {
+    marginTop: 36,
+    marginBottom: 152,
+    fontWeight: 400,
+    fontSize: 27,
+    lineHeight: '35px',
+    color: colors.navy.navBar,
+    opacity: 0.76,
+    [theme.breakpoints.down('md')]: {
+      marginBottom: 172
+    },
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: 203
+    },
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: 167
+    }
+  }
+}))
 
 export default function Custom404 () {
   const classes = useStyles()
 
   return <>
-
     <Grid className={classes.root} container direction="column" justifyContent="center" alignItems="center">
       <Grid item>
         <Typography className={classes.title}>
