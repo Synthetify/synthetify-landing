0b44233e362565eb81b5ee3d31252b32d4822c9c
Adrian <adrianwojciak99@gmail.com>
2021-12-10 19:21:54 +0100

    vercel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,11 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Features from '@components/HomePageSections/Features/Features'
 import TopBanner from '@components/HomePageSections/TopBanner/TopBanner'
 import Partners from '@components/HomePageSections/Partners/Partners'
 import ListedAssetsContainer from 'src/containers/ListedAssetsContainer/ListedAssetsContainer'
 import HomePageStatisticsContainer from 'src/containers/HomePageStatisticsContainer/HomePageStatisticsContainer'
-import { useRouter } from 'next/router'
 
 export default function Home() {
-  const router = useRouter()
-  useEffect(() => {
-    if (router.pathname.length && !(/^\/$/.test(router.pathname))) {
-      router.push('/').then(() => {}, () => {})
-    }
-  }, [router])
-
   return (
     <>
       <TopBanner />
