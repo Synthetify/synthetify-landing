02786d0f3b3bef5eacf83035b24396c706981a47
Adrian <adrianwojciak99@gmail.com>
2022-01-19 16:07:05 +0100

    version updated

--- next.config.js
+++ next.config.js
@@ -1,14 +1,31 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
+const withPlugins = require('next-compose-plugins')
+const optimizedImages = require('next-optimized-images')
 
-module.exports = withPlugins([[
-  optimizedImages, {
-    handleImages: ['svg', 'png']
+module.exports = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        handleImages: ['svg', 'png']
+      }
+    ]
+  ],
+  {
+    reactStrictMode: true,
+    trailingSlash: false,
+    images: {
+      disableStaticImages: true
+    },
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          net: false,
+          tls: false,
+          crypto: false
+        }
+      }
+
+      return config
+    }
   }
-]], {
-  reactStrictMode: true,
-  trailingSlash: false,
-  images: {
-    disableStaticImages: true,
-  }
-})
+)
